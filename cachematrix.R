c66c08252c0db35870b98fcae294b2f1fbe94107
Sam Kundu <s.kundu88@gmail.com>
2015-02-16 22:45:27 -0600

    completed programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## [Put comments here that describe what your functions do]
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # assign value of x and inverse to cache variables
+  mymatrix <<- x
+  inv <<- solve(x)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  #if matrix is unchanged, get cache of calculated inverse
+  if (identical(mymatrix,x)==T){
+    xinv <- inv
+    print("got cached value")
+  }
+  else{  #if matrix has changed, compute inverse
+  xinv <- solve(x)
+  }
 }
